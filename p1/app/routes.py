4e5ef352f16cfdc2b2ff418b744653eabc4047a8
Sergio <sergiohg818@gmail.com>
2022-10-13 17:48:08 +0200

    registro y login

--- p1/app/routes.py
+++ p1/app/routes.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+#local host y creación de archivos
+#barra de busqueda y resultados
+#archivo user data extension
+from curses.ascii import isalnum
 from doctest import testfile
+from pickle import FALSE
+import re
 from app import app
 from flask import render_template, request, url_for, redirect, session
 import json
 import os
 import sys
+import hashlib
+import random
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -16,17 +24,29 @@ def index():
     catalogue_data = open(os.path.join(app.root_path,'catalogue/inventario.json'), encoding="utf-8").read()
     catalogue = json.loads(catalogue_data)
 
-    if 'purchase' in request.form:
+    if 'details_film' in request.form:
+
+        detail =list()
+
+        for i in catalogue['peliculas']:
+            if i['id'] == int(request.form['details_film']):
+                detail.append(i)
+
+        
+
+        return render_template('details.html', title = "Details", movies=detail)
+        
+    
+    if 'add_to_cart' in request.form:
+        
         cart_data = open(os.path.join(app.root_path,'catalogue/cart.json'), encoding="utf-8").read()
         cart = json.loads(cart_data)
-       
-
-
+    
         cart_dict = dict(cart)
 
         aux_id = dict()
 
-        aux_id['id'] = int(request.form['purchase'])
+        aux_id['id'] = int(request.form['add_to_cart'])
 
         cart_dict['peliculas'] += [aux_id]
 
@@ -34,8 +54,6 @@ def index():
         cart_f = open(os.path.join(app.root_path,'catalogue/cart.json'), "w", encoding="utf-8")
         cart_f.write(json.dumps(cart_dict, indent=4))
         cart_f.close()
-        
-    
   
     
     return render_template('index.html', title = "Home", movies=catalogue['peliculas'])
@@ -45,14 +63,26 @@ def login():
     # doc sobre request object en http://flask.pocoo.org/docs/1.0/api/#incoming-request-data
     if 'username' in request.form:
         # aqui se deberia validar con fichero .dat del usuario
-        if request.form['username'] == 'pp':
-            session['usuario'] = request.form['username']
-            session.modified=True
-            # se puede usar request.referrer para volver a la pagina desde la que se hizo login
-            return redirect(url_for('index'))
-        else:
+        if os.path.isdir(os.path.join(app.root_path,'../../../si1users/' + request.form['username'])) == True:
+            
+
+
+            
+            if 'password' in request.form:
+                
+                user = open(os.path.join(app.root_path,'../../../si1users/' + request.form['username'] + '/userdata'), encoding="utf-8").read()
+                user_info = json.loads(user)
+               
+                if request.form['password'] == user_info['password']:
+                
+                    session['usuario'] = request.form['username']
+                    session.modified=True
+
+                    # se puede usar request.referrer para volver a la pagina desde la que se hizo login
+                    return redirect(url_for('index'))
+
             # aqui se le puede pasar como argumento un mensaje de login invalido
-            return render_template('login.html', title = "Sign In")
+        return render_template('login.html', title = "Sign In")
     else:
         # se puede guardar la pagina desde la que se invoca 
         session['url_origen']=request.referrer
@@ -149,3 +179,79 @@ def cart():
     
     return render_template('cart.html', title = "Home", movies=list_catalogue)
 
+
+@app.route('/details', methods=['GET', 'POST'])
+def details():
+
+    
+
+    return render_template('details.html', title = "Home")
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+
+    regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+
+    dict_user = dict()
+
+    if os.path.isdir(os.path.join(app.root_path,'../../../si1users')) == False:
+        os.mkdir(os.path.join(app.root_path,'../../../si1users'))
+
+    if 'username' in request.form:
+        if len(request.form['username']) >= 5:
+            for i in request.form['username']:
+                if (i == ' ' or i == '[' or i == '@' or i == '_' or i == '!' or i =='#' 
+                or i == '$' or i == '%' or i == '^' or i == '&' or i == '*' or i == '('
+                or i == ')' or i == '<' or i == '>' or i == '?' or i == '/' or i == '\\'
+                or i == '|' or i == '}' or i =='{' or i =='~' or i==':' or i ==']'):
+                    return render_template('register.html', title = "Home")
+
+            if os.path.isdir(os.path.join(app.root_path,'../../../si1users/' + request.form['username'])) == True:
+                return render_template('register.html', title = "Home")
+
+            os.mkdir(os.path.join(app.root_path,'../../../si1users/' + request.form['username']))
+            
+            dict_user['username'] = request.form['username']
+
+            if 'email' in request.form:
+
+                if(re.fullmatch(regex, request.form['email'])):
+                    
+                    dict_user['email'] = request.form['email']
+
+                    if 'adress' in request.form:
+                        if len(request.form['adress']) <= 50:
+                            
+                            dict_user['adress'] = request.form['adress']
+
+
+                            if 'password' in request.form:
+                                if len(request.form['password']) >= 6:
+
+                                    if 'confirm_password' in request.form:
+                                        if request.form['password'] == request.form['confirm_password']:
+                                            
+                                            contra = hashlib.sha3_384()
+                                            contra.update(request.form['password'].encode('utf-8'))
+                                           
+                                            dict_user['password'] = request.form['password']
+                                            
+
+                                            if 'credit_card' in request.form:
+                                                if len(request.form['credit_card']) == 16:
+                                                    
+                                                    dict_user['credit_card'] = request.form['credit_card']
+                                                    
+                                                    dict_user['saldo'] = random.randint(0, 50)
+
+                                                    user_f = open(os.path.join(app.root_path,'../../../si1users/' + request.form['username'] + '/userdata'), "w", encoding="utf-8")
+                                                    user_f.write(json.dumps(dict_user, indent=4))
+                                                    user_f.close()
+        
+                                                    return render_template('index.html', title = "Home")
+
+        else:            
+            return render_template('register.html', title = "Home")
+        
+
+    return render_template('register.html', title = "Home")
