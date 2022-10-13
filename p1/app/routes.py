3ebca022311bc31a04d92ea745445ae45f34ba62
Sergio <sergiohg818@gmail.com>
2022-10-14 01:51:32 +0200

    contraseña

--- p1/app/routes.py
+++ p1/app/routes.py
@@ -24,6 +24,36 @@ def index():
     catalogue_data = open(os.path.join(app.root_path,'catalogue/inventario.json'), encoding="utf-8").read()
     catalogue = json.loads(catalogue_data)
 
+    list_search = list()
+
+    if 'search' in request.form:
+
+        if 'genre' in request.form:
+
+            if request.form['genre'] == str("all"):
+                if request.form['search'] != '':
+
+                    for i in catalogue['peliculas']:
+                        if (request.form['search'].casefold() == str(i['titulo']).casefold() or 
+                        request.form['search'].casefold() == str(i['director']).casefold()):
+                            list_search.append(i)
+                else:
+                    return render_template('index.html', title = "Film Search", movies=catalogue['peliculas'])
+            else:
+                    if request.form['search'] != '':
+                        for i in catalogue['peliculas']:
+                            if ((request.form['search'].casefold() == str(i['titulo']).casefold() or 
+                                request.form['search'].casefold() == str(i['director']).casefold()) and
+                                request.form['genre'].casefold() == str(i['categoria']).casefold()):
+                                list_search.append(i)
+                    else:
+                        for i in catalogue['peliculas']:
+                            if (request.form['genre'].casefold() == str(i['categoria']).casefold()):
+                                list_search.append(i)
+
+
+        return render_template('index.html', title = "Film Search", movies=list_search)
+
     if 'details_film' in request.form:
 
         detail =list()
@@ -131,45 +161,6 @@ def logout():
     return redirect(url_for('index'))
 
 
-@app.route('/seeker', methods=['GET', 'POST'])
-def seeker():
-
-    catalogue_data = open(os.path.join(app.root_path,'catalogue/inventario.json'), encoding="utf-8").read()
-    catalogue = json.loads(catalogue_data)
-    
-     # doc sobre request object en http://flask.pocoo.org/docs/1.0/api/#incoming-request-data
-
-    list_search = list()
-
-    if 'search' in request.form:
-
-        if 'genre' in request.form:
-
-            if request.form['genre'] == str("all"):
-                if request.form['search'] != '':
-
-                    for i in catalogue['peliculas']:
-                        if (request.form['search'].casefold() == str(i['titulo']).casefold() or 
-                        request.form['search'].casefold() == str(i['director']).casefold()):
-                            list_search.append(i)
-                else:
-                    return render_template('seeker.html', title = "Film Search", movies=catalogue['peliculas'])
-            else:
-                    if request.form['search'] != '':
-                        for i in catalogue['peliculas']:
-                            if ((request.form['search'].casefold() == str(i['titulo']).casefold() or 
-                                request.form['search'].casefold() == str(i['director']).casefold()) and
-                                request.form['genre'].casefold() == str(i['categoria']).casefold()):
-                                list_search.append(i)
-                    else:
-                        for i in catalogue['peliculas']:
-                            if (request.form['genre'].casefold() == str(i['categoria']).casefold()):
-                                list_search.append(i)
-
-
-    return render_template('seeker.html', title = "Film Search", movies=list_search)
-
-  
 @app.route('/cart', methods=['GET', 'POST'])
 def cart():
     print (url_for('static', filename='css/si1.css'), file=sys.stderr)
